5a4823672f0a0942b58319d7f8b4727f64448812
Declan <declanchua15@gmail.com>
2026-04-10 17:21:14 +0800

    Fixed PR errors

--- packages/db/src/migrations/0052_membership_org_sort.sql
+++ packages/db/src/migrations/0052_membership_org_sort.sql
@@ -1,4 +1,25 @@
 ALTER TABLE "company_memberships"
+  ADD COLUMN IF NOT EXISTS "reports_to_membership_id" uuid;
+
+DO $$ BEGIN
+ IF NOT EXISTS (
+  SELECT 1
+  FROM pg_constraint
+  WHERE conname = 'company_memberships_reports_to_membership_id_company_memberships_id_fk'
+ ) THEN
+  ALTER TABLE "company_memberships"
+    ADD CONSTRAINT "company_memberships_reports_to_membership_id_company_memberships_id_fk"
+    FOREIGN KEY ("reports_to_membership_id")
+    REFERENCES "public"."company_memberships"("id")
+    ON DELETE no action
+    ON UPDATE no action;
+ END IF;
+END $$;
+
+CREATE INDEX IF NOT EXISTS "company_memberships_company_reports_to_membership_idx"
+  ON "company_memberships" ("company_id", "reports_to_membership_id");
+
+ALTER TABLE "company_memberships"
   ADD COLUMN IF NOT EXISTS "org_sort" integer NOT NULL DEFAULT 0;
 
 WITH ranked AS (
